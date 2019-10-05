d521636f60be4b4258ec541b049e0558e27265b9
Victor do Prado <victorprado123@gmail.com>
2019-10-05 13:13:49 -0300

    Home Started and Login/Registe System Making JSON with data

--- server/login.js
+++ server/login.js
@@ -8,11 +8,11 @@ app.use(bp.urlencoded({
 
 app.post('/usuarios',(req, res) =>{
     console.log(req.body)
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join("C:\Users\Pichau\Desktop\DescubraOMundo\src\html" + '/index.html', { root: __dirname }));
 })
 app.post('/registro',(req, res) =>{
     console.log(req.body)
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join("C:\Users\Pichau\Desktop\DescubraOMundo\src\html" + "/index.html", { root: __dirname }));
 })
 
 app.listen(3003)
